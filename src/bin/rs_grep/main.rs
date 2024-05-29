f2bd6151b518838b3317d8c08f6120e3330170f7
rory42edwards <rory42edwards@gmail.com>
2024-05-29 01:56:45 +0100

    Add better error handling, and separate code into more modular components

--- src/bin/rs_grep/main.rs
+++ src/bin/rs_grep/main.rs
@@ -1,12 +1,23 @@
 use std::env;
-use std::fs; // needed to handle files
+use std::process;
 
-fn main(){
+use command_line_utils::Config; // bring the Config type from library crate into binary crate's scope
+
+fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
-    
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+
+    if let Err(e) = command_line_utils::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+
 }
